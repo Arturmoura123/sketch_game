c2ef3057a47ac963288f5436a3f08a41ea713128
Arturmoura123 <artur.moura.07@gmail.com>
2023-12-05 10:12:20 +0000

    grid systems change size with user prompt

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 const grid_container = document.querySelector('.grid_container');
+const btn = document.querySelector(".btn")
+
+btn.addEventListener("click", () => {
+    
+    let userInput = prompt("Please enter the number of squares you want");
+    const parsedInput = parseInt(userInput);
+ 
+    if (!isNaN(parsedInput) && parsedInput > 0 && parsedInput <100) {
+        while (grid_container.firstChild) {
+            grid_container.removeChild(grid_container.firstChild);
+        }
+        
+        createGrid(parsedInput);
+            } else {
+        alert("Invalid input. Please enter a valid number greater than 0.");
+    }
+    }
+)
 
 
-
-
-function createGrid(){
-    for (let i = 0; i < 256; i++) {
+function createGrid(number){
+    for (let i = 0; i < number; i++) {
         const grid_square = document.createElement('div');
         const hover_effect = document.createElement("div");
         hover_effect.setAttribute('class', 'hover-effect'); 
         grid_square.appendChild(hover_effect);
         grid_container.appendChild(grid_square);
-    }
+        }
 }
-
-createGrid();
-
